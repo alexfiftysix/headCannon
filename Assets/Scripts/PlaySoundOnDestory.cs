9ae7819c6f0acdea641fc152ddc422445ce384fa
Alex Miller <qfhanvq77ughizuf74fxfhicssunktnutn6vbkhmy3hj2rrqaigq>
2022-11-22 08:01:55 +1000

    Turn down that explosion

--- Assets/Scripts/PlaySoundOnDestory.cs
+++ Assets/Scripts/PlaySoundOnDestory.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class PlaySoundOnDestory : MonoBehaviour
 {
     public AudioClip audioClip;
+    [Range(0, 1)]public float volume = 0.5f;
 
     private void OnDestroy()
     {
         var empty = new GameObject($"{gameObject.name}-DestroyAudio", typeof(AudioSource));
         empty.transform.position = transform.position;
         var emptyAudio = empty.GetComponent<AudioSource>();
+        emptyAudio.volume = volume;
         emptyAudio.PlayOneShot(audioClip);
         Destroy(empty, audioClip.length);
     }
